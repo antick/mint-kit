f1620b3d758ac90501ca59c1174d7a30ffde9c0a
Pankaj Sanam <pankaj.sanam@gmail.com>
2022-09-29 23:53:08 +0530

    removed ts configs from mint-express

--- apps/mint-express/jest.config.ts
+++ apps/mint-express/jest.config.ts
@@ -2,11 +2,6 @@
 export default {
   displayName: 'mint-express',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': 'ts-jest',

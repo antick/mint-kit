cd18015160bf476b9697b185c15f6d6da57d7450
Pankaj Sanam <pankaj.sanam@gmail.com>
2022-09-29 23:42:42 +0530

    added mongoose

--- apps/mint-express/src/app/user/controllers/validators/auth.validator.js
+++ apps/mint-express/src/app/user/controllers/validators/auth.validator.js
@@ -43,4 +43,4 @@ export default {
   refreshTokens,
   register,
   login
-}
+};
--- apps/mint-nest/src/app/app.module.ts
+++ apps/mint-nest/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
-
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+const credentials = `${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}`;
+const host = process.env.MONGO_DB_HOST;
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(`mongodb://${credentials}@${host}/?authSource=admin&ssl=false`),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
+
 export class AppModule {}
